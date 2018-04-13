58a917e571e68e8022e58aba872187d86fd682a6
erwingm <erwin.guerra.mamani@gmail.com>
2018-04-12 22:04:10 -0500

    Ok

---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+    'resources/assets/plantilla/css/font-awesome.min.css',
+    'resources/assets/plantilla/css/simple-line-icons.min.css',
+    'resources/assets/plantilla/css/main.css'
+], 'public/css/plantilla.css')
+.scripts([
+    'resources/assets/plantilla/js/jquery-3.2.1.min.js',
+    'resources/assets/plantilla/js/popper.min.js',
+    'resources/assets/plantilla/jsbootstrap.min.js',
+    'resources/assets/plantilla/js/chart.js',
+    'resources/assets/plantilla/js/pace.min.js',
+    'resources/assets/plantilla/js/main.js'
+],'public/js/plantilla.js');
